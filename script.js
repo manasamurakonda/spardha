134216be6f2844b7a98b53609da8bfb60d5f7e70
manasa <murakondamanasa@gmail.com>
2025-03-31 02:17:10 +0530

    modifications

--- script.js
+++ script.js
@@ -9,13 +9,4 @@ function showSlides() {
         slides[slideIndex - 1].style.display = "block";
         setTimeout(showSlides, 2000); // Auto change every 2 seconds
     }
-    function changeSlide(n) {
-        slideIndex += n - 1;
-        if (slideIndex >= slides.length) { slideIndex = 0; }
-        if (slideIndex < 0) { slideIndex = slides.length - 1; }
-        for (let i = 0; i < slides.length; i++) {
-            slides[i].style.display = "none";
-        }
-        slides[slideIndex].style.display = "block";
-    }
 showSlides();
